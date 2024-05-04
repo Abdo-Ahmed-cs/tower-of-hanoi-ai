05641cfeda91625d22d7dea7e04f7082990fb87b
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2024-05-04 18:04:39 +0300

    tower of hanoi game rules and drawing

---
+++ constants.py
@@ -0,0 +1,51 @@
+import pygame
+
+#window properties
+WIDTH = 800
+HEIGHT = 600
+
+TITLE = "Tower Of Hanoi AI"
+ICON = pygame.image.load("game-logo.png")
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GOLD = (239, 229, 51)
+BLUE = (78,162,196)
+GREY = (170, 170, 170)
+GREEN = (77, 206, 145)
+
+# Define stations dimensions
+ST_WIDTH = 200
+ST_HEIGHT = 25
+ST_SPACING = 50
+ST_PADDING = 50
+ST_OFFSET = 50
+
+# Define font
+pygame.font.init()
+font = pygame.font.SysFont(None, 21)
+
+# Rods
+ROD_WIDTH = 10
+ROD_HEIGHT = 300
+ROD_COLOR = GREY
+ROD_Y = HEIGHT - ROD_HEIGHT - (50 + ST_HEIGHT)
+
+#indecator
+IND_WIDTH = 10
+IND_HEIGHT = IND_WIDTH
+IND_COLOR = RED
+IND_OFFSET = 30
+
+# Number of disks
+NUM_DISKS = 3
+DISK_WIDTH = 150
+DISK_HEIGHT = 20
+DISK_COLOR = BLUE
+DISK_SPACING = 5
+
+disks = [[(WIDTH // 4 - DISK_WIDTH // 2, HEIGHT - (i + 1) * DISK_HEIGHT),
+          (WIDTH // 4 + DISK_WIDTH // 2, HEIGHT - i * DISK_HEIGHT)] for i in range(NUM_DISKS)]
+
---
+++ game.py
@@ -0,0 +1,192 @@
+import sys
+import pygame
+from constants import *
+
+
+# initializing our game display window
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(TITLE)
+pygame.display.set_icon(ICON)
+
+screen.fill(WHITE)
+
+
+class Disc:
+    def __init__(self, size, color, rod, position):
+        self.size = size
+        self.color = color
+        self.rod = rod
+        self.position = position
+
+    def draw(self):
+        disc_rect = pygame.Rect(self.position[0], self.position[1] - DISK_HEIGHT, self.size, DISK_HEIGHT)
+        pygame.draw.rect(screen, self.color, disc_rect)
+class Game():
+    def __init__(self, num_discs):
+        self.num_discs = num_discs
+        self.rods = [[], [], []]
+        self.init_discs()
+
+        self.current_pos = WIDTH // 4 - ST_WIDTH // 4 - ROD_WIDTH // 2
+        self.current_rod = 1
+        self.selected_disc = None
+        print(self.rods)
+    def draw_stations(self):
+        # Create rectangles
+        rect1 = pygame.Rect(ST_PADDING, HEIGHT - ST_OFFSET - ST_HEIGHT, ST_WIDTH, ST_HEIGHT)
+        rect2 = pygame.Rect(ST_PADDING + ST_WIDTH + ST_SPACING, HEIGHT - ST_OFFSET - ST_HEIGHT, ST_WIDTH, ST_HEIGHT)
+        rect3 = pygame.Rect(ST_PADDING + 2 * (ST_WIDTH + ST_SPACING), HEIGHT - ST_OFFSET - ST_HEIGHT, ST_WIDTH, ST_HEIGHT)
+
+        # Set up text
+        text1 = font.render("Start", True, WHITE)
+        text2 = font.render("", True, WHITE)
+        text3 = font.render("Finish", True, WHITE)
+
+        pygame.draw.rect(screen, GREEN, rect1)
+        pygame.draw.rect(screen, GREEN, rect2)
+        pygame.draw.rect(screen, GREEN, rect3)
+
+        text1_rect = text1.get_rect(center=rect1.center)
+        text2_rect = text2.get_rect(center=rect2.center)
+        text3_rect = text3.get_rect(center=rect3.center)
+
+        # Draw text
+        screen.blit(text1, text1_rect)
+        screen.blit(text2, text2_rect)
+        screen.blit(text3, text3_rect)
+
+    def create_indicator(self):
+        indicator_top = (self.current_pos + IND_WIDTH // 2, HEIGHT - IND_OFFSET - IND_HEIGHT)
+        indicator_left = (self.current_pos, HEIGHT - IND_OFFSET)
+        indicator_right = (self.current_pos + IND_WIDTH, HEIGHT - IND_OFFSET)
+        pygame.draw.polygon(screen, RED, [indicator_top, indicator_left, indicator_right])
+    def move_indicator(self, direction):
+        if direction == 'left' and self.current_rod > 1:
+            self.current_rod -= 1
+            self.current_pos -= (ST_WIDTH + ST_SPACING)
+            # Update the indicator position
+            self.create_indicator()
+
+        elif direction == 'right' and self.current_rod < 3:
+            self.current_rod += 1
+            self.current_pos += (ST_WIDTH + ST_SPACING)
+            # Update the indicator position
+            self.create_indicator()
+
+        self.redraw_scene()
+    def draw_rods(self):
+        rod1_x = WIDTH // 4 - ROD_WIDTH // 2 - (ST_WIDTH / 4)
+        rod2_x = WIDTH // 2 - ROD_WIDTH // 2
+        rod3_x = 3 * WIDTH // 4 - ROD_WIDTH // 2 + (ST_WIDTH / 4)
+
+        # Create rod rectangles
+        rod1 = pygame.Rect(rod1_x, ROD_Y, ROD_WIDTH, ROD_HEIGHT)
+        rod2 = pygame.Rect(rod2_x, ROD_Y, ROD_WIDTH, ROD_HEIGHT)
+        rod3 = pygame.Rect(rod3_x, ROD_Y, ROD_WIDTH, ROD_HEIGHT)
+
+        # Draw rods
+        pygame.draw.rect(screen, ROD_COLOR, rod1)
+        pygame.draw.rect(screen, ROD_COLOR, rod2)
+        pygame.draw.rect(screen, ROD_COLOR, rod3)
+
+    def init_discs(self):
+        for i in range(self.num_discs, 0, -1):
+            size = i * (DISK_WIDTH // self.num_discs)
+            color = (20, 20, 255 - i * (200 // self.num_discs))
+            position_x = WIDTH // 4 - ST_WIDTH // 4 - size // 2
+            position_y = HEIGHT - (self.num_discs - i + 1) * (DISK_HEIGHT + DISK_SPACING) - ST_OFFSET - (ST_HEIGHT / 2)
+            disc = Disc(size, color, 1, (position_x, position_y))
+            self.rods[0].append(disc)
+
+    def move_top_disc(self, direction):
+
+        current_rod = self.rods[self.current_rod - 1]
+
+        if direction == 'up' and len(current_rod) != 0 and self.selected_disc is None:
+            # Move the disc up
+            self.selected_disc = current_rod[-1]
+            print(self.selected_disc.size)
+            self.selected_disc.position = (
+                self.selected_disc.position[0],
+                200
+            )
+
+        elif direction == 'down' and self.selected_disc is not None:
+            if len(current_rod) != 0 and self.selected_disc.size > current_rod[-1].size:
+                return
+            # Move the disc down
+
+            self.rods[self.selected_disc.rod - 1].remove(self.selected_disc)
+
+            self.selected_disc.position = (
+                self.selected_disc.position[0],
+                500 - len(current_rod) * (DISK_HEIGHT) - DISK_SPACING * (len(current_rod) - 2)
+            )
+
+            self.selected_disc.rod = self.current_rod
+            current_rod.append(self.selected_disc)
+            self.selected_disc = None
+
+            print([len(self.rods[0]), len(self.rods[1]), len(self.rods[2])])
+        elif self.selected_disc is not None:
+            if direction == 'left':
+                new_position_x = self.selected_disc.position[0] - (ST_WIDTH + ST_SPACING)
+                if new_position_x >= 0:
+                    self.selected_disc.position = (new_position_x, self.selected_disc.position[1])
+            elif direction == 'right':
+                new_position_x = self.selected_disc.position[0] + (ST_WIDTH + ST_SPACING)
+                if new_position_x + self.selected_disc.size <= WIDTH:
+                    self.selected_disc.position = (new_position_x, self.selected_disc.position[1])
+        self.redraw_scene()
+    def draw_discs(self):
+        for rod in self.rods:
+            for disc in rod:
+                disc.draw()
+
+    def calculate_position(self, rod):
+        if rod == 0:
+            return WIDTH // 4 - ST_WIDTH // 4 - (self.rods[rod][-1].size // 2 if self.rods[rod] else 0)
+        elif rod == 1:
+            return WIDTH // 2 - (self.rods[rod][-1].size // 2 if self.rods[rod] else 0)
+        else:
+            return 3 * WIDTH // 4 - ROD_WIDTH // 2 + ST_WIDTH // 4 - (
+                self.rods[rod][-1].size // 2 if self.rods[rod] else 0)
+    def main_loop(self):
+        clock = pygame.time.Clock()
+        running = True
+
+        while running:
+            clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.move_indicator('left')
+                        self.move_top_disc('left')
+                    elif event.key == pygame.K_RIGHT:
+                        self.move_indicator('right')
+                        self.move_top_disc('right')
+                    elif event.key == pygame.K_UP:
+                        self.move_top_disc('up')
+                    elif event.key == pygame.K_DOWN:
+                        self.move_top_disc('down')
+
+            self.redraw_scene()
+
+        pygame.quit()
+        sys.exit()
+
+    def redraw_scene(self):
+        screen.fill(WHITE)
+        self.draw_stations()
+        self.create_indicator()
+        self.draw_rods()
+        self.draw_discs()
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    game = Game(6)
+    game.main_loop()

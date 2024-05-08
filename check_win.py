fa184f2f935716555ab6674d51bd64674c85e9c9
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2024-05-08 22:13:48 +0300

    fixing check_win bug

--- check_win.py
+++ check_win.py
@@ -1,3 +1,5 @@
+import sys
+
 import pygame
 
 def check_win(rods, num_discs):
@@ -8,7 +10,12 @@ def display_win_message(screen, score):
     alert_font = pygame.font.Font(None, 50)
     alert_text = alert_font.render("You Win in " + str(score) + " steps!", True, (0, 0, 0))
     alert_rect = alert_text.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+
+    esc_text = alert_font.render("Press Esc", True, (0, 0, 0))
+    esc_rect = alert_text.get_rect(center=(screen.get_width() // 2 + 70, screen.get_height() // 2 + 100))
+
     screen.blit(alert_text, alert_rect)
+    screen.blit(esc_text, esc_rect)
     pygame.display.flip()
 
 def press_any_key():
@@ -20,5 +27,5 @@ def press_any_key():
                     return "main_menu"  # Return "main_menu" if Esc key is pressed
                  # Return if any other key is pressed
             elif event.type == pygame.QUIT:
-                return
+                sys.exit()
 
